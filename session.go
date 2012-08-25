6081c48fc7298d3365f28c21a69085654993280e
borislav nikolov <jack@brokensociety.org>
2012-08-25 20:58:13 +0300

    gofmt

--- session.go
+++ session.go
@@ -1,32 +1,34 @@
 // package session provides simple http.Cookie based session management with database/sql storage
 package session
+
 import (
-	"fmt"
-	"time"
-	"encoding/gob"
-	"encoding/base64"
 	"bytes"
 	"crypto/rand"
-	"database/sql"	
+	"database/sql"
+	"encoding/base64"
+	"encoding/gob"
+	"fmt"
 	"net/http"
+	"time"
 )
-var (
-	CookieKey string = "go.session"
-	CookieValueLen int = 254
-	CookieExpireInSeconds int = 60
-	CookieSecure bool = false
-	CookieHttpOnly bool = false
-	CookieDomain string = "localhost"
-	CookiePath string = "/"
-	db *sql.DB
-	table string
-)
-type SessionObject struct {
-	Id string // unique idenfifier with CookieValueLen len
-	Data map[string]interface{} // actual data
-	Stamp int64 // last-upate time stamp
-}
 
+var (
+	CookieKey             string = "go.session"
+	CookieValueLen        int    = 254
+	CookieExpireInSeconds int    = 60
+	CookieSecure          bool   = false
+	CookieHttpOnly        bool   = false
+	CookieDomain          string = "localhost"
+	CookiePath            string = "/"
+	db                    *sql.DB
+	table                 string
+)
+
+type SessionObject struct {
+	Id    string                 // unique idenfifier with CookieValueLen len
+	Data  map[string]interface{} // actual data
+	Stamp int64                  // last-upate time stamp
+}
 
 // session.Init() with sql.DB and table name, 
 // returns error if it is unable to create the session table
@@ -36,74 +38,81 @@ type SessionObject struct {
 //		// or
 //		db, _ := sql.Open("mysql", "user:pass@tcp(192.168.0.1:3306)/app")
 //		err = session.Init(db,"session")
-func Init(_db *sql.DB,_table string) (error){
+func Init(_db *sql.DB, _table string) error {
 	db = _db
 	table = _table
-	query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%s` (id varchar(%d) UNIQUE, data blob, stamp bigint)",table,CookieValueLen + 1)
-	_,err := db.Exec(query); if err != nil {
-		fmt.Printf("failed to create table %s: %s",table, err.Error())
+	query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%s` (id varchar(%d) UNIQUE, data blob, stamp bigint)", table, CookieValueLen+1)
+	_, err := db.Exec(query)
+	if err != nil {
+		fmt.Printf("failed to create table %s: %s", table, err.Error())
 	}
 	return err
 }
 
-// extracts cookie value from http.Request.Cookie(CookieKey)
-// then creates new session object with that cookie value using NewWithId(ident)
-// finally it stores that id in http.ResponseWriter
-// example:
-// 	db, _ := sql.Open("sqlite3", "./foo.db")
-// 	defer db.Close()
-// 	session.Init(db,"session")
-// 	session.CookieKey = "go.is.awesome"
-// 	session.CookieDomain = "localhost"
-
-// 	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
-// 		s := session.New(w,r)
-// 		s.Set("list_of_numbers",[]int{1,2,3,4})
-// 		fmt.Fprintf(w,"stored key list_of_numbers in session: %s",s.Id)
-// 	})
-// 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
-// 		s := session.New(w,r)
-// 		x,_ := s.Get("list_of_numbers")
-// 		fmt.Fprintf(w,"extracted: %#v for key: list_of_numbers in session: %s",x,s.Id)
-// 	})
-// 	http.ListenAndServe("localhost:8080", nil)
+/*
+extracts cookie value from http.Request.Cookie(CookieKey)
+then creates new session object with that cookie value using NewWithId(ident)
+finally it stores that id in http.ResponseWriter
+example:
+	db, _ := sql.Open("sqlite3", "./foo.db")
+	defer db.Close()
+	session.Init(db,"session")
+	session.CookieKey = "go.is.awesome"
+	session.CookieDomain = "localhost"
+	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
+		s := session.New(w,r)
+		s.Set("list_of_numbers",[]int{1,2,3,4})
+		fmt.Fprintf(w,"stored key list_of_numbers in session: %s",s.Id)
+	})
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		s := session.New(w,r)
+		x,_ := s.Get("list_of_numbers")
+		fmt.Fprintf(w,"extracted: %#v for key: list_of_numbers in session: %s",x,s.Id)
+	})
+	http.ListenAndServe("localhost:8080", nil)
+*/
 func New(w http.ResponseWriter, r *http.Request) (s *SessionObject) {
-	cookie,_ := r.Cookie(CookieKey)
+	cookie, _ := r.Cookie(CookieKey)
 	var ident string
-	if cookie != nil { ident = cookie.Value }
+	if cookie != nil {
+		ident = cookie.Value
+	}
 	s = find_or_create(ident)
 	t := time.Now().Add(time.Duration(CookieExpireInSeconds) * time.Second)
-	cookie = &http.Cookie {	CookieKey,s.Id,CookiePath,CookieDomain,t,t.String(),CookieExpireInSeconds,CookieSecure,CookieHttpOnly,s.Id,make([]string,0)}
-	http.SetCookie(w,cookie)
-	return 
+	cookie = &http.Cookie{CookieKey, s.Id, CookiePath, CookieDomain, t, t.String(), CookieExpireInSeconds, CookieSecure, CookieHttpOnly, s.Id, make([]string, 0)}
+	http.SetCookie(w, cookie)
+	return
 }
 
 // find_or_create by ID (requested id must be of len CookieValueLen)
-func find_or_create(id string) (*SessionObject) {
-	this := &SessionObject{id,make(map[string]interface{}),time.Now().Unix()}
+func find_or_create(id string) *SessionObject {
+	this := &SessionObject{id, make(map[string]interface{}), time.Now().Unix()}
 	if len(this.Id) != CookieValueLen {
-	    b := make([]byte, CookieValueLen * 2) 
-	    rand.Read(b)
-	    this.Id = base64.StdEncoding.EncodeToString(b)[:CookieValueLen]
+		b := make([]byte, CookieValueLen*2)
+		rand.Read(b)
+		this.Id = base64.StdEncoding.EncodeToString(b)[:CookieValueLen]
 		this.store()
 	}
 	var data []byte
 	/* XXX: there can be user-modified cookie value with len == CookieValueLen, but the value is random anyway */
-	query := fmt.Sprintf("SELECT data,stamp FROM `%s` WHERE id=? AND stamp > %d",table,time.Now().Unix() - int64(CookieExpireInSeconds))
-	err := db.QueryRow(query,this.Id).Scan(&data,&this.Stamp); if err != nil {
+	query := fmt.Sprintf("SELECT data,stamp FROM `%s` WHERE id=? AND stamp > %d", table, time.Now().Unix()-int64(CookieExpireInSeconds))
+	err := db.QueryRow(query, this.Id).Scan(&data, &this.Stamp)
+	if err != nil {
 		this.store()
 	}
-	err = deserialize(data,&this.Data); if err != nil {
+	err = deserialize(data, &this.Data)
+	if err != nil {
 		this.Data = make(map[string]interface{})
 	}
 	return this
 }
 
 // session.Expire() returns the number of expired sessions
-func Expire() (int64) {
-	query := fmt.Sprintf("DELETE FROM `%s` WHERE stamp < %d",table,time.Now().Unix() - int64(CookieExpireInSeconds))
-	res,_ := db.Exec(query)
-	rows,err := res.RowsAffected(); if err != nil {
+func Expire() int64 {
+	query := fmt.Sprintf("DELETE FROM `%s` WHERE stamp < %d", table, time.Now().Unix()-int64(CookieExpireInSeconds))
+	res, _ := db.Exec(query)
+	rows, err := res.RowsAffected()
+	if err != nil {
 		return 0
 	}
 	return rows
@@ -117,35 +126,38 @@ func Expire() (int64) {
 // 		s.Set("x",map[interface{}]string{5:"x","w":"x2",5.0:"five"})
 // example: 
 // 		s.Set("list_of_numbers",[]int{1,2,3,4})
-func (this *SessionObject) Set(k string, v interface{}) (*SessionObject){
+func (this *SessionObject) Set(k string, v interface{}) *SessionObject {
 	this.Data[k] = v
 	this.store()
 	return this
 }
+
 // returns true/false if key exists in the session or not
-func (this *SessionObject) Has(k string) (bool) {
-	_,ok := this.Get(k)
+func (this *SessionObject) Has(k string) bool {
+	_, ok := this.Get(k)
 	return ok
 }
+
 // returns value of type interface{} and true/false for key existance
-func (this *SessionObject) Get(k string) (interface {},bool) {
-	v,ok := this.Data[k]
-	return v,ok
+func (this *SessionObject) Get(k string) (interface{}, bool) {
+	v, ok := this.Data[k]
+	return v, ok
 }
 
 func (this *SessionObject) store() {
 	this.Stamp = time.Now().Unix()
-	b,err := serialize(this.Data);
+	b, err := serialize(this.Data)
 	if err != nil {
 		this.err(err)
 	}
-	_,err = db.Exec("REPLACE INTO `" + table + "`(id,data,stamp) VALUES(?,?,?)",this.Id,b,this.Stamp); if err != nil {
+	_, err = db.Exec("REPLACE INTO `"+table+"`(id,data,stamp) VALUES(?,?,?)", this.Id, b, this.Stamp)
+	if err != nil {
 		this.err(err)
 	}
 }
 
 func (this *SessionObject) err(err error) {
-	fmt.Printf("error: %s: %s\n",this.Id,err.Error())
+	fmt.Printf("error: %s: %s\n", this.Id, err.Error())
 }
 
 // Copyright 2012 The Gorilla Authors. All rights reserved.
--- session_test.go
+++ session_test.go
@@ -1,28 +1,38 @@
 package session
+
 import (
-	"testing"
-	_ "github.com/mattn/go-sqlite3"
-	"database/sql"	
-	"fmt"
-	"reflect"
-	"os"
-	"time"
+	"database/sql"
 	"encoding/gob"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"reflect"
+	"testing"
+	"time"
 )
-func compare(a reflect.Value,b reflect.Value) bool {
+
+func compare(a reflect.Value, b reflect.Value) bool {
 	switch a.Kind() {
-	case reflect.Slice,reflect.Array:
-		if a.Len() != b.Len() { return false }
+	case reflect.Slice, reflect.Array:
+		if a.Len() != b.Len() {
+			return false
+		}
 		for i := 0; i < a.Len(); i++ {
-			if !compare(a.Index(i),b.Index(i)) { return false }
+			if !compare(a.Index(i), b.Index(i)) {
+				return false
+			}
 		}
 		return true
 	case reflect.Map:
-		if a.Len() != b.Len() { return false }
-   		for _, k := range a.MapKeys() {
-   			if !compare(a.MapIndex(k),b.MapIndex(k)) { return false }
-   		}
-   		return true
+		if a.Len() != b.Len() {
+			return false
+		}
+		for _, k := range a.MapKeys() {
+			if !compare(a.MapIndex(k), b.MapIndex(k)) {
+				return false
+			}
+		}
+		return true
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		return a.Int() == b.Int()
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
@@ -44,46 +54,48 @@ func TestNew(t *testing.T) {
 		return
 	}
 	defer os.Remove("./foo.db")
-	err = Init(db,"session")
+	err = Init(db, "session")
 	if err != nil {
 		t.Errorf("Failed to insert record:", err)
 		return
 	}
-	s := find_or_create("x") 
-	if (s.Id == "x") {
+	s := find_or_create("x")
+	if s.Id == "x" {
 		t.Errorf("id must not be 'x'")
 	}
 	gob.Register(map[string]int{})
 	gob.Register(map[string]string{})
 	gob.Register(map[int]string{})
 	gob.Register(map[interface{}]string{})
-	values := []interface{}{5,5.0,"x","§",[]string{"x§§RDd∂ßƒ¥˙∆©ˆø˙¨ˆå¥ß∂ˆ¨åß∂˙©","y"},-1,-0.1,1<<30, 
-							map[string]int{"a":5},
-							map[string]string{"x":"aaa","y":"bbbb"},
-							map[int]string{5:"a",6:"n"},
-							map[interface{}]string{5:"x","w":"x2",5.0:"five"},
-							[]string{}} 
-	c := func(x *SessionObject,set bool) {
+	values := []interface{}{5, 5.0, "x", "§", []string{"x§§RDd∂ßƒ¥˙∆©ˆø˙¨ˆå¥ß∂ˆ¨åß∂˙©", "y"}, -1, -0.1, 1 << 30,
+		map[string]int{"a": 5},
+		map[string]string{"x": "aaa", "y": "bbbb"},
+		map[int]string{5: "a", 6: "n"},
+		map[interface{}]string{5: "x", "w": "x2", 5.0: "five"},
+		[]string{}}
+	c := func(x *SessionObject, set bool) {
 		i := 0
-		for _,v := range values {
-			key := fmt.Sprintf("v_%d",i) 
+		for _, v := range values {
+			key := fmt.Sprintf("v_%d", i)
 			if set {
-				x.Set(key,v)
+				x.Set(key, v)
 			}
-			_v,_ := x.Get(key);
-			if !compare(reflect.ValueOf(_v),reflect.ValueOf(v)) {
-				t.Errorf("%s: stored( %#v ) != extracted( %#v )",key,v,_v)
+			_v, _ := x.Get(key)
+			if !compare(reflect.ValueOf(_v), reflect.ValueOf(v)) {
+				t.Errorf("%s: stored( %#v ) != extracted( %#v )", key, v, _v)
 			}
 			i++
 		}
 	}
-	c(s,true)
-	c(find_or_create(s.Id),false); created_sessions++
-	b := find_or_create("x"); created_sessions++
-	if (b.Id == s.Id) {
+	c(s, true)
+	c(find_or_create(s.Id), false)
+	created_sessions++
+	b := find_or_create("x")
+	created_sessions++
+	if b.Id == s.Id {
 		t.Errorf("generated ID must not be the same as the old one")
 	}
-	for i:= 0; i< 10 ;i++ {
+	for i := 0; i < 10; i++ {
 		_ = find_or_create("x")
 		created_sessions++
 	}
@@ -91,6 +103,6 @@ func TestNew(t *testing.T) {
 	time.Sleep(1000000000)
 	expired := Expire()
 	if expired != created_sessions {
-		t.Errorf("expected to expire %d sessions but expired %d",created_sessions,expired)
+		t.Errorf("expected to expire %d sessions but expired %d", created_sessions, expired)
 	}
 }
